ef37b67d172f881eeaca8777296d3e9754ff0593
Asia Thomas <Asiaa@Asias-MacBook-Air.local>
2017-08-13 20:19:31 -0500

    add warm-up

--- app/src/main/java/tmosq/com/pt/viewModel/WorkoutViewModel.java
+++ app/src/main/java/tmosq/com/pt/viewModel/WorkoutViewModel.java
@@ -10,15 +10,19 @@ import tmosq.com.pt.activity.WorkoutActivity;
 import tmosq.com.pt.helper.ExerciseSplitter;
 import tmosq.com.pt.model.Exercise;
 
+import static java.math.BigDecimal.ZERO;
 import static tmosq.com.pt.helper.ExerciseSplitter.WORK_OUT_LENGTH;
+import static tmosq.com.pt.model.exercise_support_enums.WorkOutType.WARM_UP_AND_COOL_OFF;
 
 public class WorkoutViewModel {
     private static final Integer NUMBER_OF_REPS = 10;
-    private static final Double NUMBER_OF_SETS = 3.0;
-    private static final Double SECONDS_TO_REST = 30.0 / 60.0;
+    private static final Double NUMBER_OF_SETS_FOR_REGULAR_WORKOUT = 3.0;
+    private static final Double NUMBER_OF_SETS_FOR_COOL_OFF_AND_WARM_UP_WORKOUT = 2.0;
+    private static final Double SECONDS_TO_REST_FOR_REGULAR_WORKOUT = 30.0 / 60.0;
+    private static final Double SECONDS_TO_REST_FOR_COOL_OFF_AND_WARM_UP_WORKOUT = 15.0 / 60.0;
     private static final Double PADDING_TIME = 2.0;
 
-    private final Collection<Exercise> filteredExercises;
+    protected Collection<Exercise> filteredExercises;
     private BigDecimal lengthOfWorkout;
 
     public WorkoutViewModel(WorkoutActivity workoutActivity) {
@@ -32,7 +36,17 @@ public class WorkoutViewModel {
     }
 
     public String warmUpRoutine() {
-        return "";
+        StringBuilder stringBuilder = new StringBuilder();
+        BigDecimal minutesForCoolOffAndWarmUpRegiment = BigDecimal.valueOf(5.0);
+
+        for (Exercise currentExercise : filteredExercises) {
+            if (currentExercise.getWorkOutType().equals(WARM_UP_AND_COOL_OFF) && minutesForCoolOffAndWarmUpRegiment.compareTo(ZERO) != -1) {
+                minutesForCoolOffAndWarmUpRegiment.subtract(estimatedTimeToDoCoolOffAndWarmUpExercise(currentExercise));
+                stringBuilder.append(currentExercise.getWorkout());
+            }
+        }
+
+        return stringBuilder.toString();
     }
 
     public int coolOffWorkoutVisibility() {
@@ -47,7 +61,7 @@ public class WorkoutViewModel {
         StringBuilder stringBuilder = new StringBuilder();
 
         for (Exercise currentExercise : filteredExercises) {
-            while (lengthOfWorkout.compareTo(BigDecimal.ZERO) != -1) {
+            while (lengthOfWorkout.compareTo(ZERO) != -1) {
                 estimatedTimeToDoWorkoutCycle(currentExercise);
                 stringBuilder.append(currentExercise.getWorkout())
                         .append(": 3 sets of 10 reps\nRest for 30 seconds in between each set\n\n");
@@ -58,10 +72,17 @@ public class WorkoutViewModel {
         return stringBuilder.toString();
     }
 
+    private BigDecimal estimatedTimeToDoCoolOffAndWarmUpExercise(Exercise currentExercise) {
+        BigDecimal timeOfExerciseRegiment = BigDecimal.valueOf(currentExercise.getAverageSecondsPerRep() * NUMBER_OF_REPS / 60.0);
+        timeOfExerciseRegiment = timeOfExerciseRegiment.multiply(BigDecimal.valueOf(NUMBER_OF_SETS_FOR_COOL_OFF_AND_WARM_UP_WORKOUT));
+        timeOfExerciseRegiment = timeOfExerciseRegiment.add(BigDecimal.valueOf(SECONDS_TO_REST_FOR_COOL_OFF_AND_WARM_UP_WORKOUT));
+        return timeOfExerciseRegiment;
+    }
+
     private void estimatedTimeToDoWorkoutCycle(Exercise currentExercise) {
-        BigDecimal workOutCycleTime = BigDecimal.valueOf(currentExercise.getAverageSecondsPerRep() * NUMBER_OF_REPS / 60);
-        workOutCycleTime = workOutCycleTime.add(BigDecimal.valueOf(SECONDS_TO_REST));
-        workOutCycleTime = workOutCycleTime.multiply(BigDecimal.valueOf(NUMBER_OF_SETS));
+        BigDecimal workOutCycleTime = BigDecimal.valueOf(currentExercise.getAverageSecondsPerRep() * NUMBER_OF_REPS / 60.0);
+        workOutCycleTime = workOutCycleTime.add(BigDecimal.valueOf(SECONDS_TO_REST_FOR_REGULAR_WORKOUT));
+        workOutCycleTime = workOutCycleTime.multiply(BigDecimal.valueOf(NUMBER_OF_SETS_FOR_REGULAR_WORKOUT));
         workOutCycleTime = workOutCycleTime.add(BigDecimal.valueOf(PADDING_TIME));
         lengthOfWorkout = lengthOfWorkout.subtract(workOutCycleTime);
     }
--- app/src/test/java/tmosq/com/pt/viewModel/WorkoutViewModelTest.java
+++ app/src/test/java/tmosq/com/pt/viewModel/WorkoutViewModelTest.java
@@ -1,13 +1,111 @@
 package tmosq.com.pt.viewModel;
 
+import android.content.Intent;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import tmosq.com.pt.BuildConfig;
+import tmosq.com.pt.activity.WorkoutActivity;
+import tmosq.com.pt.model.Exercise;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static tmosq.com.pt.helper.ExerciseSplitter.WORK_OUT_LENGTH;
+import static tmosq.com.pt.model.exercise_support_enums.BodyFocus.ABDOMINALS;
+import static tmosq.com.pt.model.exercise_support_enums.Difficulty.ADVANCED;
+import static tmosq.com.pt.model.exercise_support_enums.Difficulty.BASIC;
+import static tmosq.com.pt.model.exercise_support_enums.Difficulty.INTERMEDIATE;
+import static tmosq.com.pt.model.exercise_support_enums.Equipment.BANDS;
+import static tmosq.com.pt.model.exercise_support_enums.Equipment.BICYCLE;
+import static tmosq.com.pt.model.exercise_support_enums.Equipment.CHAIR;
+import static tmosq.com.pt.model.exercise_support_enums.Equipment.FOAM_ROLL;
+import static tmosq.com.pt.model.exercise_support_enums.WorkOutType.BODY;
+import static tmosq.com.pt.model.exercise_support_enums.WorkOutType.WARM_UP_AND_COOL_OFF;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, manifest = "src/main/AndroidManifest.xml", sdk = 21)
 public class WorkoutViewModelTest {
     private WorkoutViewModel workoutViewModel;
+
+    @Before
+    public void setUp() throws Exception {
+        Intent intent = new Intent();
+        intent.putExtra(WORK_OUT_LENGTH, 60);
+
+        WorkoutActivity workoutActivity = Robolectric.buildActivity(WorkoutActivity.class).withIntent(intent).create().get();
+
+        workoutViewModel = new WorkoutViewModel(workoutActivity);
+    }
+
+    @Test
+    public void warmUpRoutine_onlyGetCoolOffAndWarmUpExercises() throws Exception {
+        String hyperRocks = "hyper rocks";
+        String lowTides = "low tides";
+        String jetPacks = "jet packs";
+        String morningKicks = "morning kicks";
+
+        Exercise warmUpExerciseOne = Exercise.builder().equipment(BANDS).difficulty(BASIC).workOutType(WARM_UP_AND_COOL_OFF).forTime(false)
+                .bodyFocus(ABDOMINALS).averageSecondsPerRep(5.0).workout(hyperRocks).build();
+
+        Exercise warmUpExerciseTwo = Exercise.builder().equipment(CHAIR).difficulty(INTERMEDIATE).workOutType(WARM_UP_AND_COOL_OFF).forTime(false)
+                .bodyFocus(ABDOMINALS).averageSecondsPerRep(5.0).workout(lowTides).build();
+
+        Exercise nonWarmUpExerciseOne = Exercise.builder().equipment(FOAM_ROLL).difficulty(ADVANCED).workOutType(BODY).forTime(false)
+                .bodyFocus(ABDOMINALS).averageSecondsPerRep(5.0).workout(jetPacks).build();
+
+        Exercise nonWarmUpExerciseTwo = Exercise.builder().equipment(BICYCLE).difficulty(ADVANCED).workOutType(BODY).forTime(false)
+                .bodyFocus(ABDOMINALS).averageSecondsPerRep(5.0).workout(morningKicks).build();
+
+        workoutViewModel.filteredExercises = newArrayList(warmUpExerciseOne, warmUpExerciseTwo, nonWarmUpExerciseOne, nonWarmUpExerciseTwo);
+
+        assertTrue(workoutViewModel.warmUpRoutine().contains(hyperRocks));
+        assertTrue(workoutViewModel.warmUpRoutine().contains(lowTides));
+        assertFalse(workoutViewModel.warmUpRoutine().contains(jetPacks));
+        assertFalse(workoutViewModel.warmUpRoutine().contains(morningKicks));
+    }
+
+    @Test
+    public void warmUpRoutine_shouldNotHaveAnyMoreWarmUpAndCoolOffExercisesAfterFiveMinutes() throws Exception {
+        String hyperRocks = "hyper rocks";
+        String lowTides = "low tides";
+        String jetPacks = "jet packs";
+        String morningKicks = "morning kicks";
+        String starDays = "star days";
+        String uberWalks = "uber walks";
+
+        Exercise warmUpExerciseOne = Exercise.builder().equipment(BANDS).difficulty(BASIC).workOutType(WARM_UP_AND_COOL_OFF).forTime(false)
+                .bodyFocus(ABDOMINALS).averageSecondsPerRep(10.0).workout(hyperRocks).build();
+
+        Exercise warmUpExerciseTwo = Exercise.builder().equipment(CHAIR).difficulty(INTERMEDIATE).workOutType(WARM_UP_AND_COOL_OFF).forTime(false)
+                .bodyFocus(ABDOMINALS).averageSecondsPerRep(10.0).workout(lowTides).build();
+
+        Exercise warmUpExerciseThree = Exercise.builder().equipment(FOAM_ROLL).difficulty(ADVANCED).workOutType(WARM_UP_AND_COOL_OFF).forTime(false)
+                .bodyFocus(ABDOMINALS).averageSecondsPerRep(10.0).workout(jetPacks).build();
+
+        Exercise warmUpExerciseFour = Exercise.builder().equipment(BICYCLE).difficulty(ADVANCED).workOutType(WARM_UP_AND_COOL_OFF).forTime(false)
+                .bodyFocus(ABDOMINALS).averageSecondsPerRep(10.0).workout(morningKicks).build();
+
+        Exercise warmUpExerciseFive = Exercise.builder().equipment(BICYCLE).difficulty(ADVANCED).workOutType(WARM_UP_AND_COOL_OFF).forTime(false)
+                .bodyFocus(ABDOMINALS).averageSecondsPerRep(10.0).workout(starDays).build();
+
+        Exercise warmUpExerciseSix = Exercise.builder().equipment(BICYCLE).difficulty(ADVANCED).workOutType(WARM_UP_AND_COOL_OFF).forTime(false)
+                .bodyFocus(ABDOMINALS).averageSecondsPerRep(10.0).workout(uberWalks).build();
+
+        workoutViewModel.filteredExercises = newArrayList(warmUpExerciseOne, warmUpExerciseTwo,
+                warmUpExerciseThree, warmUpExerciseFour, warmUpExerciseFive, warmUpExerciseSix);
+
+        String warmUpRoutine = workoutViewModel.warmUpRoutine();
+        assertFalse(warmUpRoutine.contains(hyperRocks) &&
+                warmUpRoutine.contains(lowTides) &&
+                warmUpRoutine.contains(jetPacks) &&
+                warmUpRoutine.contains(morningKicks) &&
+                warmUpRoutine.contains(starDays) &&
+                warmUpRoutine.contains(uberWalks));
+    }
 }
