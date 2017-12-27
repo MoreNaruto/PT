bf08d0ac44b91e75e920247a6070e856acd13fa6
MoreNaruto <morenaruto007@gmail.com>
2017-12-27 11:50:00 -0600

    Removed unnecessary test and code in preworkoutviewmodel

--- app/src/main/java/tmosq/com/pt/viewModel/PreWorkOutViewModel.java
+++ app/src/main/java/tmosq/com/pt/viewModel/PreWorkOutViewModel.java
@@ -2,57 +2,18 @@ package tmosq.com.pt.viewModel;
 
 import android.databinding.BaseObservable;
 import android.databinding.ObservableBoolean;
-import android.databinding.ObservableField;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import lombok.Getter;
-import tmosq.com.pt.model.exercise_support_enums.Difficulty;
-import tmosq.com.pt.model.exercise_support_enums.Equipment;
-import tmosq.com.pt.model.exercise_support_enums.WorkoutRegiment;
 
 public class PreWorkOutViewModel {
     @Getter
-    private ObservableField<String> workoutDifficulty = new ObservableField<>(Difficulty.BASIC.getDifficultyNameAlias());
-    @Getter
-    private ObservableField<String> workOutRegiment = new ObservableField<>(WorkoutRegiment.CARDIO.getWorkOutRegimentNameAlias());
-    @Getter
     private ObservableBoolean hasPartner = new ObservableBoolean(false);
-    @Getter
-    private ObservableField<List<String>> excludedEquipments = new ObservableField<>((List<String>) new ArrayList<String>());
 
     public BaseObservable makeWorkoutButtonClicked = new BaseObservable();
-    public ObservableField<String> selectAllEquipmentClicked = new ObservableField<>("Select all");
 
     public PreWorkOutViewModel() {
-    }
-
-    public void setWorkOutRegiment(String workOutRegiment) {
-        this.workOutRegiment.set(workOutRegiment);
-    }
-
-    public void setWorkOutDifficulty(String workOutDifficulty) {
-        this.workoutDifficulty.set(workOutDifficulty);
-    }
-
-    public View.OnClickListener clickEquipmentCheckBox() {
-        return new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                CheckBox equipmentCheckBox = (CheckBox) view;
-                String equipment = Equipment.fromResourceCheckBoxId(view.getId()).getEquipmentNameAlias();
-                if (equipmentCheckBox.isChecked()) {
-                    excludedEquipments.get().add(equipment);
-                } else {
-                    excludedEquipments.get().remove(equipment);
-                }
-            }
-        };
     }
 
     public View.OnClickListener clickPartnerBox() {
@@ -71,22 +32,6 @@ public class PreWorkOutViewModel {
             @Override
             public void onClick(View view) {
                 makeWorkoutButtonClicked.notifyChange();
-            }
-        };
-    }
-
-    public View.OnClickListener clickSelectAllEquipmentCheckBox() {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView selectAllTextView = (TextView) view;
-                String selectAllTextViewString = selectAllTextView.getText().toString();
-                if (selectAllTextViewString.equals("Select all")){
-                    excludedEquipments.set(Equipment.allEquipmentNameAliases());
-                } else {
-                    excludedEquipments.get().clear();
-                }
-                selectAllEquipmentClicked.set(selectAllTextViewString);
             }
         };
     }
--- app/src/test/java/tmosq/com/pt/viewModel/PreWorkOutViewModelTest.java
+++ app/src/test/java/tmosq/com/pt/viewModel/PreWorkOutViewModelTest.java
@@ -1,64 +1,24 @@
 package tmosq.com.pt.viewModel;
 
 import android.widget.CheckBox;
-import android.widget.TextView;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
 import tmosq.com.pt.R;
-import tmosq.com.pt.activity.PreWorkoutActivity;
 
-import static junit.framework.Assert.assertFalse;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
 import static org.robolectric.RuntimeEnvironment.application;
-import static tmosq.com.pt.model.exercise_support_enums.Difficulty.ADVANCED;
-import static tmosq.com.pt.model.exercise_support_enums.Equipment.BANDS;
-import static tmosq.com.pt.model.exercise_support_enums.WorkoutRegiment.POWER_LIFTING;
 
 @RunWith(RobolectricTestRunner.class)
 public class PreWorkOutViewModelTest {
-
     private PreWorkOutViewModel preWorkOutViewModel;
-    private PreWorkoutActivity preWorkoutActivity;
 
     @Before
     public void setUp() throws Exception {
-        preWorkoutActivity = mock(PreWorkoutActivity.class);
         preWorkOutViewModel = new PreWorkOutViewModel();
-    }
-
-    @Test
-    public void setWorkOutRegiment_getWorkOutRegiment() throws Exception {
-        preWorkOutViewModel.setWorkOutRegiment(POWER_LIFTING.getWorkOutRegimentNameAlias());
-
-        assertEquals(preWorkOutViewModel.getWorkOutRegiment().get(), POWER_LIFTING.getWorkOutRegimentNameAlias());
-    }
-
-
-    @Test
-    public void setWorkOutDifficulty_getDifficulty() throws Exception {
-        preWorkOutViewModel.setWorkOutDifficulty(ADVANCED.getDifficultyNameAlias());
-
-        assertEquals(preWorkOutViewModel.getWorkoutDifficulty().get(), ADVANCED.getDifficultyNameAlias());
-    }
-
-    @Test
-    public void clickEquipmentCheckBox_getExcludedEquipment() throws Exception {
-        preWorkoutActivity = Robolectric.buildActivity(PreWorkoutActivity.class).create().get();
-        preWorkOutViewModel = new PreWorkOutViewModel();
-
-        CheckBox bandsCheckBox = new CheckBox(application);
-        bandsCheckBox.setId(R.id.bands_checkbox);
-        bandsCheckBox.setChecked(true);
-
-        preWorkOutViewModel.clickEquipmentCheckBox().onClick(bandsCheckBox);
-        assertEquals(preWorkOutViewModel.getExcludedEquipments().get().get(0), BANDS.getEquipmentNameAlias());
     }
 
     @Test
@@ -70,35 +30,5 @@ public class PreWorkOutViewModelTest {
 
         preWorkOutViewModel.clickPartnerBox().onClick(partnerCheckBox);
         assertTrue(preWorkOutViewModel.getHasPartner().get());
-    }
-
-    @Test
-    public void clickSelectAllEquipmentCheckBox_whenSelectAllIsClicked_shouldHighlightAllExcludedEquipmentItems() throws Exception {
-        preWorkoutActivity = Robolectric.buildActivity(PreWorkoutActivity.class).create().get();
-        preWorkOutViewModel = new PreWorkOutViewModel();
-
-        TextView defaultValue = new TextView(application);
-        defaultValue.setId(R.id.select_all_text_view);
-        defaultValue.setText(R.string.select_all);
-
-        preWorkOutViewModel.clickSelectAllEquipmentCheckBox().onClick(defaultValue);
-
-        assertFalse(preWorkOutViewModel.getExcludedEquipments().get().isEmpty());
-        assertEquals(preWorkOutViewModel.selectAllEquipmentClicked.get(), "Select all");
-    }
-
-    @Test
-    public void clickSelectAllEquipmentCheckBox_whenUnSelectAllIsClicked_shouldUnHighlightAllExcludedEquipmentItems() throws Exception {
-        preWorkoutActivity = Robolectric.buildActivity(PreWorkoutActivity.class).create().get();
-        preWorkOutViewModel = new PreWorkOutViewModel();
-
-        TextView defaultValue = new TextView(application);
-        defaultValue.setId(R.id.select_all_text_view);
-        defaultValue.setText(R.string.unselect_all);
-
-        preWorkOutViewModel.clickSelectAllEquipmentCheckBox().onClick(defaultValue);
-
-        assertTrue(preWorkOutViewModel.getExcludedEquipments().get().isEmpty());
-        assertEquals(preWorkOutViewModel.selectAllEquipmentClicked.get(), "Unselect all");
     }
 }
